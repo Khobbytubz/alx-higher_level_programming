ac4d6354b4fd402a0093b4547cf9515659ee2360
khobbytubz <khobbytubz@gmail.com>
2023-03-13 23:11:27 +0000

    i love c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,9 @@
-f (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
-} else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
+#!/usr/bin/node
+let count = Number(process.argv[2]);
+if (isNaN(count)) {
+  console.log('Missing Number of occurrences');
+} else if (count > 0) {
+  for (count; count > 0; --count) {
     console.log('C is fun');
-    i++;
   }
 }
